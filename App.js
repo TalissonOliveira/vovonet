a1ef964f5b3611977fb02e76c06da4749a34f591
Jonathan Rodrigues <jonathanlr2000@gmail.com>
2021-01-11 21:08:28 -0300

    dependencias

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Routes from './src/routes/routes'
+// import Routes from './src/routes/routes'
+import Tab from './src/routes/tabLevelChoice'
 
 export default function App() {
   return (
-    <Routes></Routes>
+    <Tab/>
   );
 }
